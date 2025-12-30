8399d18a859ea95cd3082672f2a9a47647ba8e94
Java sauce <mohammedbrandon204@gmail.com>
2025-12-29 22:22:51 -0400

    add

---
+++ src/main/java/jerios/awareZombies/AwareZombies.java
@@ -0,0 +1,46 @@
+package jerios.awareZombies;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = AwareZombies.MODID, version = Tags.VERSION, name = "Situationally Aware Zombies", acceptedMinecraftVersions = "[1.7.10]")
+public class AwareZombies {
+
+    public static final String MODID = "awareZombies";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "jerios.awareZombies.ClientProxy", serverSide = "jerios.awareZombies.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
